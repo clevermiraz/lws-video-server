3f4cbe2cf71c4f5e93dd6c1cec80be024beeda02
MEHEDIs PC <miraz.onesoft@gmail.com>
2022-09-09 09:41:29 +0600

    transactions add

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
-const videos = require('./db_videos.json');
-const tags = require('./db_tags.json');
+const videos = require("./db_videos.json");
+const tags = require("./db_tags.json");
+const transactions = require("./db_transactions.json");
 
 module.exports = {
     videos,
-    tags
+    tags,
+    transactions,
 };
