d430ccdb564e52a3c2183a6f1fcd9afbae624405
MEHEDIs PC <miraz.onesoft@gmail.com>
2022-09-10 20:43:57 +0600

    add todo json

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const videos = require("./db_videos.json");
 const tags = require("./db_tags.json");
 const transactions = require("./db_transactions.json");
+const todos = require("./db_todos.json");
 
 module.exports = {
     videos,
     tags,
     transactions,
+    todos,
 };
